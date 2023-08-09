3d5a2ab673887fdb7a94c491efa1f7cfe65f87a2
U Naveen Babu <136116172+naveen0913@users.noreply.github.com>
2023-08-09 19:44:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -99,13 +99,12 @@ app.use(session({
     sameSite:"name",
     secure:true,
     maxAge:1000 * 60 * 60 * 24 * 7
-  }
-}))
-
-/*  store:MongoStore.create({
+  },
+   store:MongoStore.create({
     mongoUrl:process.env.MONGODB_URL,
     autoRemove:'native',
-  })*/
+   })
+}))
 
 app.post('/add', upload1.single('photo'),(req,res)=>{
  
